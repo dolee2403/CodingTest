8d7ebf20b58c6cea3fcc0a34769c89182bc3ce8b
이도 <164998147+dolee2403@users.noreply.github.com>
2024-09-12 09:12:47 +0900

    [level 0] Title: n의 배수, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+        if (2 <= num && num <= 100 && 2 <= n && n <= 9) {
+            if (num % n == 0) {
+                answer = 1;
+            } else {
+                answer = 0;
+            }
+        }
+        return answer;
+    }
+}
