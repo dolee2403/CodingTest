327ae4112c403662c6f26109c9df391e4615d881
이도 <164998147+dolee2403@users.noreply.github.com>
2024-11-05 11:18:24 +0900

    [level 0] Title: 외계행성의 나이, Time: 1.67 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,12 +1,8 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
-        // a b c d e f g h i j
-        // 0 1 2 3 4 5 6 7 8 9
         String[] strArr = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-        
         String ageStr = Integer.toString(age);
-        
         for(int i = 0; i < ageStr.length(); i++) {
             int num = Integer.parseInt(String.valueOf(ageStr.charAt(i)));
             answer += strArr[num];
