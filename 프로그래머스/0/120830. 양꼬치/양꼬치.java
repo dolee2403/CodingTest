2c60b6f1e9e78b711c86ed76eb7a6e7863f909c0
이도 <164998147+dolee2403@users.noreply.github.com>
2024-11-21 10:58:39 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int n, int k) {
-        int result = (n*12000) + ((k - (n/10)) * 2000);
-        return result;
+        int answer = (n*12000) + (k*2000) - (n/10*2000);
+        
+        return answer;
     }
 }
