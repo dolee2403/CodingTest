4bc3fd23996d12b75348d3ffb905e6e319da54ca
이도 <164998147+dolee2403@users.noreply.github.com>
2024-12-02 17:38:24 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i = 1; ; i++) {
-            int pie = n * i;
-            if(pie % 6 == 0) {
-                answer = pie/6;
+        int pizza = 6*n;
+        for(int i = 1; i <= pizza; i++) {
+            if(6 * i % n == 0) {
+                answer = i;
                 break;
             }
         }
