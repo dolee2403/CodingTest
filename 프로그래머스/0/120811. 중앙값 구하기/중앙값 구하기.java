7894ab5613ec97a173f0d3ac1f77eac41b781324
이도 <164998147+dolee2403@users.noreply.github.com>
2024-12-02 14:04:28 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.57 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,11 +1,7 @@
-import java.util.*;
+import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
         Arrays.sort(array);
-        int answer = 0;
-        if(array.length % 2 == 1) {
-            answer = array[array.length/2];
-        }
-        return answer;
+        return array[array.length/2];
     }
 }
