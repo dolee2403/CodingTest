8e0fadaaba121b9b6a1d8feb79924d8e4d743ca2
이도 <164998147+dolee2403@users.noreply.github.com>
2024-12-03 10:26:45 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,9 +1,16 @@
 class Solution {
     public int[] solution(int money) {
+        int[] answer = new int[2];
         int aa = 5500;
-        int cup = money/aa;
+        int jan = money/aa;
         int rate = money%aa;
-        
-        return new int[] {cup, rate};
+        if (money >= 5500) {
+            answer[0] = jan;
+            answer[1] = rate;
+        } else {
+            answer[0] = 0;
+            answer[1] = rate;
+        }
+        return answer;
     }
 }
